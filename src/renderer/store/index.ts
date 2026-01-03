36a9876c38ec6073e7158d34bda68199e80aafee
Jhon Erick <jhon3rick@gmail.com>
2026-01-03 16:57:45 -0500

    feat: configure logger for state changes

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -4,12 +4,22 @@ import tagsReducer from './tagsSlice';
 import commandsReducer from './commandsSlice';
 import configReducer from './configSlice';
 
+import stateChangeLogger from "./middlewares/stateChangeLogger";
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const combineReducers = {
+  projects: projectsReducer,
+  tags: tagsReducer,
+  commands: commandsReducer,
+  config: configReducer,
+};
+
 export const store = configureStore({
-  reducer: {
-    projects: projectsReducer,
-    tags: tagsReducer,
-    commands: commandsReducer,
-    config: configReducer,
+  devTools: isDevelopment,
+  reducer: combineReducers,
+  middleware: (getDefaultMiddleware) =>{
+    const base = getDefaultMiddleware({ serializableCheck: false });
+    return isDevelopment ? base.concat(stateChangeLogger()) : base;
   },
 });
 
---
+++ src/renderer/store/middlewares/stateChangeLogger.ts
@@ -0,0 +1,26 @@
+import type { Middleware } from "@reduxjs/toolkit";
+
+export default (): Middleware => {
+  return (storeApi) => (next) => (action) => {
+    const prevState = storeApi.getState();
+    const result = next(action);
+    const nextState = storeApi.getState();
+
+    console.log("StateChangeLogger Middleware triggered");
+
+    if (prevState !== nextState) {
+      const actionType =
+        typeof action === "object" && action && "type" in action
+          ? String((action as { type: unknown }).type)
+          : "UNKNOWN_ACTION";
+
+      console.groupCollapsed(`🧠 Redux: ${actionType}`);
+      console.log("Prev state:", prevState);
+      console.log("Next state:", nextState);
+      console.log("Action:", action);
+      console.groupEnd();
+    }
+
+    return result;
+  };
+};
